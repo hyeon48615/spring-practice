e81bb0308b0cbcf034cd679edab15ae48d8b3c6a
hyeon48615 <hyeon48615@gmail.com>
2025-10-21 00:14:45 +0900

    chore: [spring-prepare] Group 변경 (net.sparta -> com.sparta)

--- spring-prepare/src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ spring-prepare/src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,4 +1,4 @@
-package net.sparta.springprepare.controller;
+package com.sparta.springprepare.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- spring-prepare/src/test/java/com/sparta/springprepare/SpringPrepareApplicationTests.java
+++ spring-prepare/src/test/java/com/sparta/springprepare/SpringPrepareApplicationTests.java
@@ -1,4 +1,4 @@
-package net.sparta.springprepare;
+package com.sparta.springprepare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
